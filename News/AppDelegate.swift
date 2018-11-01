f5bcc0095abc699dda6db8ab121e68af6700d356
Nidhin <epnidhinios@gmail.com>
2018-11-01 00:06:18 -0600

    Develop#5

--- News/AppDelegate.swift
+++ News/AppDelegate.swift
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       //Commit 3
       //Commit 4
       //Commit 5
+      //Commit 6
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
