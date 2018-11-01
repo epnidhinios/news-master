25050d947ecb38e3c5d773679bed38f56550b7e6
Nidhin <epnidhinios@gmail.com>
2018-10-31 20:51:31 -0600

    One

--- News/ViewController.swift
+++ News/ViewController.swift
@@ -23,7 +23,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var newshaderCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
