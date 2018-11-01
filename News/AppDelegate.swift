1f437bf5739be83a0724cbce2b51feb2fd11b119
Nidhin <epnidhinios@gmail.com>
2018-10-31 22:10:13 -0600

    Commit 4

--- News/AppDelegate.swift
+++ News/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       //Commit 1
       //Commit 2
       //Commit 3
+      //Commit 4
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
