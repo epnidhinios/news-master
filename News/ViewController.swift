7a44af6be03f525ebca76b07c01ac09e7f963caa
Nidhin <epnidhinios@gmail.com>
2018-11-01 00:22:37 -0600

    Test2

--- News/ViewController.swift
+++ News/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       //Test1
+      //Test 2
     }
 
     override func didReceiveMemoryWarning() {
